c247957f2f65e85f1ed74b9f0d9ecdb8577df70f
Dima <dimalebedev6902@gmail.com>
2022-09-08 20:24:06 +0300

    deleted unused type

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -26,22 +26,7 @@ export interface IFields {
   password: string;
 }
 
-export type TGoogleResponse = {
-  aud?: string;
-  azp?: string;
-  email?: string;
-  email_verified?: boolean;
-  exp?: number;
-  family_name?: string;
-  given_name?: string;
-  iat?: number;
-  iss?: string;
-  jti?: string;
-  name?: string;
-  nbf?: number;
-  picture?: string;
-  sub?: string;
-};
+
 
 const Register: React.FC = () => {
   const { isMember, user } = useAppSelector(selectUserData);
