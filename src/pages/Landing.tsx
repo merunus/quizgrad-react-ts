7d9fd61a85f60b5e1afb2862c8867ebcc37d5137
Dima <dimalebedev6902@gmail.com>
2022-09-08 16:39:27 +0300

    Adding framer motion to landing page

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -4,10 +4,27 @@ import vectorImage from "../assets/images/line.svg";
 import { Link } from "react-router-dom";
 import { Logo } from "../components";
 import { Endpoints } from "../models/routes";
+import { motion } from "framer-motion";
+
+const textAnimations = {
+  hidden: {
+    x: -100,
+    opacity: 0,
+  },
+  visible: (custom: number) => ({
+    x: 0,
+    opacity: 1,
+    transition: { delay: custom * 0.1 },
+  }),
+};
 
 const Landing: React.FC = () => {
   return (
-    <div className="landingContainer">
+    <motion.div
+      initial="hidden"
+      whileInView="visible"
+      className="landingContainer"
+    >
       <nav>
         <Logo />
         <Link to={Endpoints.Register}>
@@ -18,23 +35,29 @@ const Landing: React.FC = () => {
       </nav>
       <div className="detailsContainer">
         <div className="details">
-          <h1>
+          <motion.h1 custom={1} variants={textAnimations}>
             Learn
             <br /> new words <br />
             for each language
-          </h1>
-          <p>We help you prepare for exams and quizes</p>
+          </motion.h1>
+          <motion.p variants={textAnimations} custom={2}>
+            We help you prepare for exams and quizes
+          </motion.p>
           <Link to={Endpoints.Register}>
-            <button className="button button--landingDetails ">
+            <motion.button
+              variants={textAnimations}
+              custom={3}
+              className="button button--landingDetails "
+            >
               Start Learning
-            </button>
+            </motion.button>
           </Link>
         </div>
         <img src={mainImage} alt="mainImage" />
       </div>
 
       <img className="landingLine" src={vectorImage} alt="vectorLine" />
-    </div>
+    </motion.div>
   );
 };
 
